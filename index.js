a5bf478a948f793b57fc8608c0947612488116a9
jenn-renken <88343948+jenn-renken@users.noreply.github.com>
2021-10-20 14:15:37 -0400

    added generateMarkdown/ability to write README file

--- index.js
+++ index.js
@@ -1,55 +1,94 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Please enter a description for your project'
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Please enter installation instructions for your project'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Please enter usage information for your project'
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Please enter testing instructions for your project'
-    },
-    {
-        type: 'list',
-        name: 'licenses',
-        message: 'Choose a license for your project:',
-        choices: ['Apache', 'Boost', 'Eclipse', 'IBM', 'ISC', 'MIT', 'Mozilla']
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Please enter your Github username'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Please enter your email'
-    }
-];
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a title!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please enter a description for your project',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please enter installation instructions for your project'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please enter usage information for your project'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please enter testing instructions for your project'
+        },
+        {
+            type: 'list',
+            name: 'licenses',
+            message: 'Choose a license for your project:',
+            choices: ['Apache', 'Boost', 'Eclipse', 'IBM', 'ISC', 'MIT', 'Mozilla']
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter your Github username',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a Github username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email'
+        }
+    ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err)
+            throw err;
+        console.log('README generated!');
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput);
+        writeToFile('./dist/README.md', generateMarkdown(userInput));
+    });
+};
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,38 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+  ###  ${data.title}
 
+  ## Description
+   ${data.description}
+
+  ## Table of Contents
+  1. Installation
+  2. Usage
+  3. License
+  4. Contributing
+  5. Tests
+  6. Questions
+ 
+  ## Installation
+   ${data.installation}
+
+  ## Usage
+   ${data.usage}
+
+  ## License
+   ${data.license}
+
+  ## Contributing
+   ${data.github}
+
+  ## Tests
+   ${data.tests}
+
+  ## Questions
+   ${data.email}
 `;
 }
 
