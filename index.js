64360419d43710374177749d358c112bf30129ed
jenn-renken <88343948+jenn-renken@users.noreply.github.com>
2021-10-20 10:33:31 -0400

    added questions array

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description for your project'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter installation instructions for your project'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter usage information for your project'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please enter testing instructions for your project'
+    },
+    {
+        type: 'list',
+        name: 'licenses',
+        message: 'Choose a license for your project:',
+        choices: ['Apache', 'Boost', 'Eclipse', 'IBM', 'ISC', 'MIT', 'Mozilla']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your Github username'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
